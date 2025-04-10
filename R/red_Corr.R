77a9a7dd564e501a4b4d816e3d52ebc878eb9a67
piledge <70260300+piledge@users.noreply.github.com>
2025-04-10 12:38:07 +0200

    Update red_Corr.R
    add dependency

--- R/red_Corr.R
+++ R/red_Corr.R
@@ -1,8 +1,8 @@
 red_Corr <- function (data, class_column = 1, cutoff = 0.8){
+  kaiserschmRn::package_install('caret')
+
   cor_matrix <- cor(data[-class_column])
-  high_corr <- findCorrelation(cor_matrix, cutoff = cutoff)
+  high_corr <- caret::findCorrelation(cor_matrix, cutoff = cutoff)
   data_littlecorr <- cbind(data[class_column], data[-high_corr])
   return(data_littlecorr)
 }
-
-
