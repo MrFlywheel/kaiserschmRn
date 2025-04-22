a4d3eb504e188d17204b070043528fc0715b677c
MrFlywheel <n.mihatsch@gmail.com>
2025-04-22 15:29:32 +0200

    -

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -5,7 +5,7 @@ missing_deps  <- setdiff(dependencies, installed_pkgs)
 
 if (length(missing_deps) > 0) {
   cat("\nMissing dependencies:", paste(missing_deps, collapse = ", "), "\n")
-  cat("Use 'pizzR::package.install(pizzR::dependencies)' to add missing packages.\n\n")
+  cat("Use 'kaiserschmRn::package.install(pizzR::dependencies)' to add missing packages.\n\n")
 } else {
   cat("All dependencies are installed.\n")
 }
