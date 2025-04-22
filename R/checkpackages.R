17f2f3ee5ed3c6bbe8521cbc8da1ba66b669bcf5
MrFlywheel <n.mihatsch@gmail.com>
2025-04-22 15:27:44 +0200

    Bugfix

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -1,11 +1,11 @@
 dependencies <- c('moments', 'lidR', 'terra', 'feather', 'piledge/pizzR', "parallel", 'doParallel', 'foreach')
 
-to_install <- !dependencies %in% rownames(installed.packages())
+installed_pkgs <- installed.packages()[, "Package"]
+missing_deps  <- setdiff(dependencies, installed_pkgs)
 
-if (any(to_install)) {
-  missing_dependencies <- as.character(dependencies[to_install])
-  cat("\n\nMissing dependencies:", paste(missing_dependencies, collapse = ", "), "\n")
-  cat("Use 'kaiserschmRn::package_install(kaiserschmRn::dependencies)' to add missing packages.\n\n")
+if (length(missing_deps) > 0) {
+  cat("\nMissing dependencies:", paste(missing_deps, collapse = ", "), "\n")
+  cat("Use 'pizzR::package.install(pizzR::dependencies)' to add missing packages.\n\n")
 } else {
   cat("All dependencies are installed.\n")
 }
