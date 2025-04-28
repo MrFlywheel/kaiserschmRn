c0c621478a6ce19459e2469f5ba663488d5e4cb2
MrFlywheel <n.mihatsch@gmail.com>
2025-04-28 13:42:54 +0200

    - typo

--- R/red_Corr.R
+++ R/red_Corr.R
@@ -1,5 +1,5 @@
 red_Corr <- function (data, class_column = 1, cutoff = 0.8){
-  kaiserschmRn::package_install('caret')
+  kaiserschmRn::package.install('caret')
 
   cor_matrix <- cor(data[-class_column])
   high_corr <- caret::findCorrelation(cor_matrix, cutoff = cutoff)
