4047365359dcefb8d76b6f3e3544b34f3b756184
MrFlywheel <n.mihatsch@gmail.com>
2025-09-23 12:21:00 +0200

    -added new function to compute surface roughness -added help for roughness_RMSE

---
+++ R/roughness_RMSE.R
@@ -0,0 +1,25 @@
+roughness_RMSE <- function(las_path, res = 1, dtm = NULL, filter_height = NULL){
+  library(lidR)
+  library(terra)
+
+  rmse_plane_function <- function(x, y, z) {
+    fit <- lm(z ~ x + y)
+    z_fit <- predict(fit)
+    residuals <- z - z_fit
+    rmse <- sqrt(mean(residuals^2, na.rm = TRUE))
+    return(rmse)}
+
+  las <- readLAS(las_path)
+  normalised <- any(head(las$Classification == 2) & (head(las$Z)>2))
+  stopifnot("las needs to be a height normalised point cloud. Please provide a path to a dtm over the variable 'dtm'" = normalised & !is.null(dtm))
+  if(!is.null(dtm)){
+    dtm <- terra::rast(dtm)
+    las <- las - dtm
+  }
+  nlas_filtered <- filter_poi(las, Z < filter_height)
+  stopifnot("specify a height above ground with variable 'filter_height' which should be still considered to compute roughness" = !is.null(filter_height))
+
+  rmse_raster <- grid_metrics(nlas_filtered, res = res, rmse_plane_function(X, Y, Z))
+  plot(rmse_raster)
+  return(rmse_raster)
+}
